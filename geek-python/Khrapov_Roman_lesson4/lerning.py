ed94c423002b53472eb0383a9e1f2dac2ecb1975
Khrapov Roman <hardrom@gmail.com>
2021-08-19 11:26:27 +0300

    Khrapov_Roman_lesson4

--- geek-python/Khrapov_Roman_lesson4/lerning.py
+++
@@ -1,8 +0,0 @@
-# import sys
-import sys
-from sys import argv
-
-file_name, work_time, hours_pay, prem = sys.argv
-
-print(f"Заработная плата {int(work_time) * int(hours_pay) + int(prem)} рублей")
-
